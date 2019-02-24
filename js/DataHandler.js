fe99ae4a9af6563d0f006fed517b1ec4f0da264d
abdi <m.abdi@aut.ac.ir>
2019-02-24 14:04:53 +0330

    userAgent,PluginsList,PlatformInfo

--- js/DataHandler.js
+++ js/DataHandler.js
@@ -5,6 +5,25 @@ import {Utils} from './Utils.js';
 var DataHandler = (function() {
     'use strict';
     
+    
+    function getUserAgent()
+    {
+        return browser.runtime.getBrowserInfo();
+    }
+
+    function getAllInstalledPlugins()
+    {
+        return browser.management.getAll();
+    }
+
+
+    function getPlatformInfo()
+    {
+        return browser.runtime.getPlatformInfo();
+    }
+    
+    
+    
     function handle(message) {
         console.log("DataHandler", message);
         Utils.notify(message);
