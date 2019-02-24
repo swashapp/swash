abaf4501ff666276698830dc2e46520df9059fa8
abdi <m.abdi@aut.ac.ir>
2019-02-24 13:56:39 +0330

    browsing modules,

--- js/modules/twitter/browsing.js
+++ js/modules/twitter/browsing.js
@@ -6,93 +6,127 @@ Twitter.browsing_extraInfoSpec= ["blocking"];
 Twitter.browsing = [
     {
         name: "post_tweet",
-        title: "",
+        title: "Post a new tweet",
         description: "",
-        
-        method: "POST",
-        url_pattern: "https://twitter.com/i/tweet/create",
-        pattern_type: "exact",
-        param: [
-            {
-                type: "form",
-                key: "status",
-                name: "text"
-            }
-        ]
-    },{
-        name: "search_hash",
-        method: "GET",
-        url_pattern: "https:\/\/twitter\.com\/hashtag\/([^?]+)\?src=hash",
-        pattern_type: "regex",
-        param: [
-            {
-                type: "regex",
-                group: 1,
-                name: "hashtag"
-            }
-        ]
+        patterns: [
+        {
+            method: "POST",
+            url_pattern: "https://twitter.com/i/tweet/create",
+            pattern_type: "exact",
+            param: [
+                {
+                    type: "form",
+                    key: "status",
+                    name: "text"
+                }
+            ]
+        }
     },{
         name: "search",
-        method: "GET",
-        url_pattern: "https:\/\/twitter\.com\/search\?q=([^&]+)&src=typd",
-        pattern_type: "regex",
-        param: [
-            {
-                type: "regex",
-                group: 1,
-                name: "q"
-            }
+        title: "Search",
+        description: "",
+        patterns: [
+        {
+            method: "GET",
+            url_pattern: "https:\/\/twitter\.com\/hashtag\/([^?]+)\?src=hash",
+            pattern_type: "regex",
+            param: [
+                {
+                    type: "regex",
+                    group: 1,
+                    name: "hashtag"
+                }
+            ]
+        },{
+            method: "GET",
+            url_pattern: "https:\/\/twitter\.com\/search\?q=([^&]+)&src=typd",
+            pattern_type: "regex",
+            param: [
+                {
+                    type: "regex",
+                    group: 1,
+                    name: "q"
+                }
+            ]
+        }
         ]
     },{
         name: "follow",
-        method: "POST",
-        url_pattern: "https://api.twitter.com/1.1/friendships/create.json",
-        pattern_type: "exact",
-        param: [
-            {
-                type: "form",
-                key: "user_id",
-                name: "user_id"
-            }
+        title: "Follow",
+        description: "",
+        patterns: [
+        {
+            method: "POST",
+            url_pattern: "https://api.twitter.com/1.1/friendships/create.json",
+            pattern_type: "exact",
+            param: [
+                {
+                    type: "form",
+                    key: "user_id",
+                    name: "user_id"
+                }
+            ]
+        }
         ]
     },{
         name: "unfollow",
-        method: "POST",
-        url_pattern: "https://api.twitter.com/1.1/friendships/destroy.json",
-        pattern_type: "exact",
-        param: [
-            {
-                type: "form",
-                key: "user_id",
-                name: "user_id"
-            }
+        title: "Unfollow",
+        description: "",
+        patterns: [
+        {
+            method: "POST",
+            url_pattern: "https://api.twitter.com/1.1/friendships/destroy.json",
+            pattern_type: "exact",
+            param: [
+                {
+                    type: "form",
+                    key: "user_id",
+                    name: "user_id"
+                }
+            ]
+        }
         ]
     },{
         name: "mute",
-        method: "POST",
-        url_pattern: "https://twitter.com/i/user/mute",
-        pattern_type: "exact",
-        param: [
-            {
-                type: "form",
-                key: "user_id",
-                name: "user_id"
-            }
+        title: "Mute",
+        description: "",
+        patterns: [
+        {
+            method: "POST",
+            url_pattern: "https://twitter.com/i/user/mute",
+            pattern_type: "exact",
+            param: [
+                {
+                    type: "form",
+                    key: "user_id",
+                    name: "user_id"
+                }
+            ]
+        }
         ]
     },{
         name: "unmute",
-        method: "POST",
-        url_pattern: "https://twitter.com/i/user/unmute",
-        pattern_type: "exact",
-        param: [
-            {
-                type: "form",
-                key: "user_id",
-                name: "user_id"
-            }
-        ]
+        title: "Unmute",
+        description: "",
+        patterns: [
+        {
+            method: "POST",
+            url_pattern: "https://twitter.com/i/user/unmute",
+            pattern_type: "exact",
+            param: [
+                {
+                    type: "form",
+                    key: "user_id",
+                    name: "user_id"
+                }
+            ]
+        }
     },{
         name: "like",
+        title: "Like",
+        description: "",
+        patterns: [
+        {
         method: "POST",
         url_pattern: "https://api.twitter.com/1.1/favorites/create.json",
         pattern_type: "exact",
@@ -103,29 +137,43 @@ Twitter.browsing = [
                 name: "tweet_id"
             }
         ]
+        }
+        ]
     },{
         name: "retweet",
-        method: "POST",
-        url_pattern: "https:\/\/twitter\.com\/i\/tweet\/html\?id=([0-9]+)&modal=retweet",
-        pattern_type: "regex",
-        param: [
-            {
-                type: "regex",
-                group: 1,
-                name: "tweet_id"
-            }
+        title: "Retweet",
+        description: "",
+        patterns: [
+        {
+            method: "POST",
+            url_pattern: "https:\/\/twitter\.com\/i\/tweet\/html\?id=([0-9]+)&modal=retweet",
+            pattern_type: "regex",
+            param: [
+                {
+                    type: "regex",
+                    group: 1,
+                    name: "tweet_id"
+                }
+            ]
+        }
         ]
     },{
         name: "visit",
-        method: "GET",
-        url_pattern: "https:\/\/twitter\.com\/([_A-Za-z0-9]+)$",
-        pattern_type: "regex",
-        param: [
-            {
-                type: "regex",
-                group: 1,
-                name: "username"
-            }
+        title: "Visit User page",
+        description: "",
+        patterns: [
+        {
+            method: "GET",
+            url_pattern: "https:\/\/twitter\.com\/([_A-Za-z0-9]+)$",
+            pattern_type: "regex",
+            param: [
+                {
+                    type: "regex",
+                    group: 1,
+                    name: "username"
+                }
+            ]
+        }
         ]
     },
     {
