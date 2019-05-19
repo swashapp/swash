a52f0e5a0740c63300338d90af13b44daca6ca01
peterx <peterx.client@gmail.com>
2019-05-20 01:26:17 +0430

    onInstalled bug fix && streamr-client bug fix

--- js/stream.js
+++ js/stream.js
@@ -3,17 +3,27 @@ import {streamrConf} from './streamConfig.js';
 
 
 var stream = (function() {
-
+	
+	const sessionTimeout = 2*60*1000;
 	var client = new StreamrClient({
 		auth:{
 			apiKey: streamrConf.API_KEY
 		}
 	})
-
+	var sessionStartTime = (new Date()).getTime()
 	// Wrap event generation and producion into this method
 	function produceNewEvent(msg) {
 	  
 	  // Produce the event to the Stream
+	  let currentTime = (new Date()).getTime();
+	  if((currentTime - sessionStartTime) > sessionTimeout) {
+		  sessionStartTime = currentTime;
+		  client = new StreamrClient({
+				auth:{
+					apiKey: streamrConf.API_KEY
+				}
+			})
+	  }
 	  console.log("produceNewEvent ", msg);
 	  client.publish(streamrConf.STREAM_ID, msg)
 		.then(() => {
