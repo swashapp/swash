3882db872a08c88f77c39bae3e209abcf5f519d1
khalilzadeh <Khalilzadeh@KHALILZADEH.Behsazan.net>
2019-05-25 17:40:23 +0430

    internal filters

---
+++ js/internalFilters.js
@@ -0,0 +1,59 @@
+var internalFilters = [
+  {
+    "value": "https://www.amazon.com/ap/signin*",
+    "type": "wildcard",
+    "internal": true
+  },
+  {
+    "value": "https://www.facebook.com/login*",
+    "type": "wildcard",
+    "internal": true
+  },
+  {
+    "value": "https://www.facebook.com/v3.2/dialog/oauth*",
+    "type": "wildcard",
+    "internal": true
+  },
+  {
+    "value": "https://login.yahoo.com/*",
+    "type": "wildcard",
+    "internal": true
+  },
+  {
+    "value": "https://accounts.google.com/*",
+    "type": "wildcard",
+    "internal": true
+  },
+  {
+    "value": "https://login.live.com/*",
+    "type": "wildcard",
+    "internal": true
+  },
+  {
+    "value": "https://login.aol.com/*",
+    "type": "wildcard",
+    "internal": true
+  },
+  {
+    "value": "https://twitter.com/login*",
+    "type": "wildcard",
+    "internal": true
+  },
+  {
+    "value": "https://twitter.com/account*",
+    "type": "wildcard",
+    "internal": true
+  },
+  {
+    "value": "http://drive.google.com/*",
+    "type": "wildcard",
+    "internal": true
+  },
+  {
+    "value": "http://docs.google.com/*",
+    "type": "wildcard",
+    "internal": true
+  }
+]
+
+export {internalFilters};
