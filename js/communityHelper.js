a13be4142f840ebecdfb445f1664f3f6507736a1
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-09-26 21:09:53 +0300

    Added earnings query and withdrawFor helpers

--- js/communityHelper.js
+++ js/communityHelper.js
@@ -53,36 +53,28 @@ var communityHelper = (function() {
 	function getBalance() {
 		if (!wallet || !provider) return;
 		let streamrContract = new ethers.Contract(communityConfig.streamrAddress, communityConfig.abi, provider);
-		return	streamrContract.balanceOf(wallet.address);
+		return streamrContract.balanceOf(wallet.address);
 	}
 
-	function getAvailableBalance() {
-		//is there a function for getting monoplaspa balance?
+	async function getAvailableBalance() {
+		if (!client) clientConnect();
+		const stats = await client.memberStats(communityConfig.communityAddress, wallet.address);
+		return stats.withdrawableEarnings;
 	}
 
-	function withrawEarnings() {
+	function withdrawEarnings() {
 		if (!wallet) return;
-		if(!client) clientConnect();
+		if (!client) clientConnect();
 		client.withdraw(communityConfig.communityAddress, wallet.address, wallet)
 		return withrawEarningsFor(wallet.address)
 	}
 
-	function withrawEarningsFor(recipient) {
+	function withdrawEarningsFor(recipient) {
 		if (!contract) return;
 
-		//GET https://streamr.com/api/v1/communities/{communityAddress}/members/{recipient}
-		const member = {
-			address: "0xdC353aA3d81fC3d67Eb49F443df258029B01D8aB",
-			earnings: "333333333333333333",
-			active: true,
-			proof: [
-				"0xd92d93e1a5532affe4f635cc97c555c39413ceecea690323cbd2d21b0d5acb8d",
-				"0x6cfa6f3735edabee127ccb3ad7dc4a9aba79993664eead578e3544b7dc58c515"
-			],
-			recordedEarnings: "333333333333333333",
-			withdrawableEarnings: "0",
-			withdrawableBlockNumber: "1",
-			frozenEarnings: "333333333333333333"
+		const member = await client.memberStats(communityConfig.communityAddress, recipient);
+		if (member.withdrawableEarnings < 1) {
+			throw new Error("Nothing to withdraw")
 		}
 
 		return contract.withdrawAll(
@@ -98,8 +90,8 @@ var communityHelper = (function() {
 		getEncryptedWallet,
 		join,
 		part,
-		withrawEarnings,
-		withrawEarningsFor,
+		withdrawEarnings,
+		withdrawEarningsFor,
 		getWalletInfo,
 		getBalance
     };
