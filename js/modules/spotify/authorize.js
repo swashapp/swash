d3bc33f7be80b1dabb941b83c747e9d8b045efdd
peterx <peterx.client@gmail.com>
2020-02-08 01:50:41 +0330

    spotify module is added

---
+++ js/modules/spotify/authorize.js
@@ -0,0 +1,30 @@
+console.log("modules/spotify/authorize.js");
+import {spotify} from './manifest.js';
+
+// TODO: remove scopes that are not necessary
+spotify.apiConfig = {
+            client_id: "39c1107e38e04d0a85546f34327e87bb",			
+            api_endpoint: "https://api.spotify.com/v1",
+            auth_url: 'https://accounts.spotify.com/authorizeh',
+            access_token_regex: "access_token=([^&]*)",
+            scopes: [   "user-read-playback-state",
+                        "playlist-read-collaborative",
+                        "user-top-read",
+                        "user-read-currently-playing",
+                        "playlist-read-private",
+                        "user-follow-read",
+						"user-read-recently-played",
+						"user-library-read"
+                        ]
+        }
+spotify.validate_token = {
+    name: "validate_token",
+    description: "",
+    method: "GET",
+    endpoint: "https://api.spotify.com/v1",
+    URI: "/me",
+    token_param_name:"id",
+    content_type: "application/x-www-form-urlencoded",
+    response_type: "json",
+    required_jpath:"$.expires_in",
+}
