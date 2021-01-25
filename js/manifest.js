d65ce02827c9fcebe95c01d6cc61982ce00b197a
AliReza Seyfpour <a.seyfpour@gmail.com>
2021-01-25 14:54:41 +0330

    SWAS-28 Change manifest description
    Change manifest description to "Get paid for your data as you browse the web. Gain control and help create a better, more dignified internet. Available now on all major browsers."

--- js/manifest.js
+++ js/manifest.js
@@ -3,7 +3,7 @@ var ssConfig = (function() {
     
     return {
         name: browser.runtime.getManifest().name,        
-        description: "Swash is a Streamr Community Product that collects information about users browsing activities and gives users the ability to sell their data through Streamr Marketplace",
+        description: "Get paid for your data as you browse the web. Gain control and help create a better, more dignified internet. Available now on all major browsers.",
         path: "/",
         is_enabled: true,
 		privacyLevel: 'auto',
