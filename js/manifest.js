b5f93cf9ec02911d19beb59021398241163fda5d
Ebrahim Khalilzadeh <ebrahim@swashapp.io>
2021-03-05 00:41:41 +0400

    modify the description

--- js/manifest.js
+++ js/manifest.js
@@ -3,7 +3,7 @@ var ssConfig = (function() {
     
     return {
         name: browser.runtime.getManifest().name,        
-        description: "Get paid for your data as you browse the web. Gain control and help create a better, more dignified internet. Available now on all major browsers.",
+        description: "Get paid for your data as you browse the web. Gain control and help create a better, more dignified internet.",
         path: "/",
         is_enabled: true,
 		privacyLevel: 'auto',
